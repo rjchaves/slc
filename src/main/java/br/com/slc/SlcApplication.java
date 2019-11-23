7fc30ddd33ed82daee91962d658f4bd4a84014a9
Raul José Chaves <rjchaves93@gmail.com>
2019-11-23 12:32:51 -0300

    Initial Commit

---
+++ src/main/java/br/com/slc/SlcApplication.java
@@ -0,0 +1,13 @@
+package br.com.slc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlcApplication.class, args);
+	}
+
+}
