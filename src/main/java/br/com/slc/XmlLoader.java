5c31ef4bbc2cd3370aa133bb6344dff67add7aed
Raul José Chaves <rjchaves93@gmail.com>
2019-11-23 22:36:19 -0300

    Corrigido erro de leitura de arquivo dentro do jar

--- src/main/java/br/com/slc/XmlLoader.java
+++ src/main/java/br/com/slc/XmlLoader.java
@@ -4,15 +4,22 @@ import br.com.slc.domain.Documento;
 import br.com.slc.repository.DocumentoRepository;
 import br.com.slc.xml.DOC;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.tomcat.util.http.fileupload.FileUtils;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ResourceUtils;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
 
 @Component
 public class XmlLoader {
@@ -26,17 +33,16 @@ public class XmlLoader {
     }
 
     @EventListener
-    public void onApplicationEvent(ContextRefreshedEvent event) throws JAXBException {
+    public void onApplicationEvent(ContextRefreshedEvent event) throws JAXBException, IOException {
         var doc = loadXml();
         var document = objectMapper.convertValue(doc, Documento.class);
         documentoRepository.save(document);
     }
 
-    public DOC loadXml() throws JAXBException {
-        File file = new File(SlcApplication.class.getClassLoader().getResource("SLC0001-modelo.xml").getFile());
+    public DOC loadXml() throws JAXBException, IOException {
+        var is = new ClassPathResource("SLC0001-modelo.xml", this.getClass().getClassLoader()).getInputStream();
         JAXBContext jaxbContext = JAXBContext.newInstance(DOC.class);
         Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
-        return (DOC) unmarshaller.unmarshal(file);
-
+        return (DOC) unmarshaller.unmarshal(is);
     }
 }
